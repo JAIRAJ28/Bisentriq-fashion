cb0a7916db72befeb9bf8b7bf64eb6ed8df38dcf
Jai raj singh <jairajsingh28july@gmail.com>
2023-02-26 05:32:03 +0530

    alldata

--- bientriq_rct101/src/Navbar/Components/Routing/routing.jsx
+++ bientriq_rct101/src/Navbar/Components/Routing/routing.jsx
@@ -2,6 +2,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home1st } from "../../../pages/Home1st";
 import { Home2nd } from "../../../pages/Home2nd";
+import { Fashion } from "../../../pages/Fashion";
+import SinglePage from "../../../pages/singlepages/singlepage1";
+import Cartpage from "../../../pages/cartpage";
+import  PrivateRoute  from "../../../pages/PrivateRoute/privateRoute";
+import { Alldress } from "../../../pages/allpages/AllDress";
+import SingleAlldress from "../../../pages/allpages/singleAlldres";
+import { Furniture } from "../../../pages/allpages/Furniture";
+import SingleFurniture from "../../../pages/allpages/allfur";
 const Allroutes = () => {
     return (
       <>
@@ -11,11 +19,23 @@ const Allroutes = () => {
       
 <Route path="/" element={<Home1st />}></Route>
 
-{/* Cart And Payment */}
 <Route path='/home2nd' element={<Home2nd />} />
-{/* <Route path='/checkout/shipping-address' element={<Checkout />} />
-<Route path='/checkout/shipping-delivery' element={<Delivery />} />
-<Route path='/checkout/payment' element={<Payment />} /> */}
+<Route path='/newAll' element={<Alldress/>}/>
+<Route path='/newAll/:id' element={<SingleAlldress/>}/>
+
+<Route path="/furniture" element={<Furniture/>}/>
+<Route path="/furniture:id" element={<SingleFurniture/>}/>
+
+<Route path="/fashion" element={<Fashion />}/>
+
+<Route path='/fashion/:id' element={<SinglePage/>}/>
+
+<Route path='/addtocart' element={
+<PrivateRoute >
+<Cartpage/>
+</PrivateRoute>
+}/>
+
 
 </Routes>
 </>
--- bientriq_rct101/src/Navbar/Components/signin/signup/signup.jsx
+++ bientriq_rct101/src/Navbar/Components/signin/signup/signup.jsx
@@ -1,7 +1,48 @@
 import { Link } from "@chakra-ui/react"
 import "./logpop.css"
+import { useState } from "react"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
+import { useNavigate } from "react-router-dom"
+import { auth } from "../../../../firbase"
+import { useContext } from "react"
+import { Authcontext } from "../../../Authcontext/contextApi"
+const Signup=({handelClosesignup,handelClick})=>{
+const [errMessg,setErr]=useState(false)
+const navigate=useNavigate()
+let [data,setData]=useState({
+  email:"",
+  mobile:"",
+  password:"",
+})
 
-const Signup=({handelClosesignup})=>{
+
+
+const HandelChange=(e)=>{
+  let val=e.target.value
+  // let name=e.target.name
+  setData({...data,[e.target.name]:val})
+}
+console.log(data)
+console.log(auth)
+const handelSubmit=(e)=>{
+  e.preventDefault()
+if(!data.email|| !data.password || !data.mobile){
+  setErr(true)
+  return
+}
+createUserWithEmailAndPassword(auth,data.email,data.password).then(async(res)=>{
+  console.log(res)
+  handelClosesignup()
+  alert("Signup SuceessFull,Please Go Ahead And Login Again")
+  handelClick()
+  navigate("/")
+}).catch((Err)=>{
+  console.log(Err)
+  alert(Err.message)
+})
+}
+
+
 
     return <>
      <div className="Signup">
@@ -10,7 +51,7 @@ const Signup=({handelClosesignup})=>{
       handelClosesignup()
     }}
     >X</div>
-    <form action="">
+    <form action="" onSubmit={handelSubmit}>
  <div className="form">
     <p className="big">Create An Account</p>
     <hr/>
@@ -18,29 +59,41 @@ const Signup=({handelClosesignup})=>{
     <div className="form-element"></div>
     <label for="">Email*</label>
     <br/>
-    <input type="email" id="email1" placeholder="Enter Email"  required/>
+    <input type="email" name="email" id="email" placeholder="Enter Email" value={data.email} onChange={HandelChange}  required/>
     <label for="">MOBILE NUMBER*</label>
     <br/>
-    <input type="number" id="numb" placeholder="Enter Mobile" min="10" max="10" required/>
+    <input type="number" name="mobile" id="email" placeholder="Enter Mobile" value={data.mobile}   onChange={HandelChange} required/>
  </div>
+ <input type="password" name="password" id="email" placeholder="Enter Password" value={data.password}  onChange={HandelChange} required/>
   <input type="checkbox"/>
   <label for="">keep me signed in <span className="animate"><button className="hov">i</button></span></label>
   
   <div>
-  <button className="submit">NEXT</button>
+    <p
+    style={{
+      color:"red"
+    }}
+    >{errMessg?"Fill All Fields":""}</p>
+  <button className="submit"
+  >NEXT</button>
 </div>
+
 <input type="checkbox"/>
 <label style={{fontSize:"12px",lineHeight: "2px"}} for="">Sign me up to receive Anthropologie offers, promotions and other commercial messages. By creating an account, I agree to Anthropologie's Terms of Service and Privacy Policy. I may unsubscribe at any time. Creating an account will not enroll you in SMS marketing.</label>
-<p style={{fontSize:"12px",lineHeight:"20px"}}>By creating an account, you agree to Anthropologie's <Link href="">terms of use</Link> and privacy policy. 
+<hr />
+<p style={{fontSize:"11px",lineHeight:"20px"}}>By creating an account, you agree to Anthropologie's <Link href="">terms of use</Link> and privacy policy. 
   Landlines, VoIP, and prepaid phones are not supported. Standard message & data rates may apply.
    Message frequency may vary. Reply HELP for help, and STOP to cancel. Contact us for more information.</p>
+  </form>
    <hr/>
   <p className="big">Already Have An Account</p>
-  <button className="signinone">
-    <Link href="">SIGN IN</Link>
+  <button className="signinone"
+  onClick={handelClick}
+  >
+   SIGN IN
   </button>
   
-</form>
+
 </div>
 
     </>
